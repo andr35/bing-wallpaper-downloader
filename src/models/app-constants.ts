dbc83c615d63338e109f5aa6868dea0ab6dcac11
Andrea Zorzi <andrea.zorzi.94@gmail.com>
2019-02-10 18:00:02 +0100

    fix base ng url

--- src/models/app-constants.ts
+++ src/models/app-constants.ts
@@ -2,7 +2,7 @@ import {homedir, tmpdir} from 'os';
 import {join} from 'path';
 
 export class AppConstants {
-  static NG_URL = 'https://www.nationalgeographic.com/photography/photo-of-the-day/_jcr_content/.gallery.json?user.testname=none';
+  static NG_URL = 'https://www.nationalgeographic.com/photography/photo-of-the-day/_jcr_content/.gallery.json';
 
   static DEFAULT_PHOTO_PATH = join(homedir(), '.wallpaper.jpg');
   static DEFAULT_PHOTO_PATH_TMP = join(tmpdir(), 'wallpaper.jpg');
